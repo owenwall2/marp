c1802df45ad32e2f7c88eff6808a793c1473240b
Mason Vari <mvari@g.clemson.edu>
2026-04-02 20:32:17 -0400

    enabling adsb and fm

--- app.py
+++ app.py
@@ -21,17 +21,27 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
+import sys
+from xmlrpc import server
+sys.modules['SimpleXMLRPCServer'] = server
+
 from PyQt5 import Qt
 from gnuradio import eng_notation
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
-from gnuradio.filter import firdes
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import zeromq
+import SimpleXMLRPCServer
+import threading
+import gnuradio.adsb as adsb
 from gnuradio import qtgui
 
 class app(gr.top_block, Qt.QWidget):
@@ -79,8 +89,6 @@ class app(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.zeromq_pub_sink_2 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://127.0.0.1:5004', 10, False, -1)
-        self.zeromq_pub_sink_1 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://127.0.0.1:5003', 10, False, -1)
         self._threshold_tool_bar = Qt.QToolBar(self)
         self._threshold_tool_bar.addWidget(Qt.QLabel('Detection Threshold' + ": "))
         self._threshold_line_edit = Qt.QLineEdit(str(self.threshold))
@@ -92,28 +100,118 @@ class app(gr.top_block, Qt.QWidget):
             self.top_grid_layout.setRowStretch(r, 1)
         for c in range(1, 2):
             self.top_grid_layout.setColumnStretch(c, 1)
+        self.zeromq_pub_sink_2 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://127.0.0.1:5004', 10, False, -1)
+        self.zeromq_pub_sink_1 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://127.0.0.1:5003', 10, False, -1)
+        self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_float, 1, 'tcp://127.0.0.1:5002', 10, False, -1)
+        self.zeromq_pub_msg_sink_0 = zeromq.pub_msg_sink('tcp://127.0.0.1:5001', 10)
+        self.xmlrpc_server_0 = SimpleXMLRPCServer.SimpleXMLRPCServer(('0.0.0.0', 5010), allow_none=True)
+        self.xmlrpc_server_0.register_instance(self)
+        self.xmlrpc_server_0_thread = threading.Thread(target=self.xmlrpc_server_0.serve_forever)
+        self.xmlrpc_server_0_thread.daemon = True
+        self.xmlrpc_server_0_thread.start()
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=12,
+                decimation=125,
+                taps=None,
+                fractional_bw=0.4)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            int(fs*150e-6), #size
+            int(fs), #samp_rate
+            "", #name
+            2 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(1.0/100.0)
+        self.qtgui_time_sink_x_0.set_y_axis(0, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_TAG, qtgui.TRIG_SLOPE_POS, 0, 1.25e-6, 0, "burst")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+        self.qtgui_time_sink_x_0.disable_legend()
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [0, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 1, 0, 1, 2)
+        for r in range(1, 2):
+            self.top_grid_layout.setRowStretch(r, 1)
+        for c in range(0, 2):
+            self.top_grid_layout.setColumnStretch(c, 1)
         self.blocks_throttle_1 = blocks.throttle(gr.sizeof_gr_complex*1, 32000,True)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, 32000,True)
+        self.blocks_selector_0 = blocks.selector(gr.sizeof_gr_complex*1,0,select_index)
+        self.blocks_selector_0.set_enabled(True)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
         self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, 512)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(1)
         self.blocks_add_xx_1 = blocks.add_vcc(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=48000*4,
+        	audio_decimation=4,
+        )
         self.analog_noise_source_x_1 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
         self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
+        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, threshold)
+        self.adsb_framer_1 = adsb.framer(fs, threshold)
+        self.adsb_demod_0 = adsb.demod(fs)
+        self.adsb_decoder_0 = adsb.decoder("Extended Squitter Only", "None", "Brief")
 
 
 
         ##################################################
         # Connections
         ##################################################
+        self.msg_connect((self.adsb_decoder_0, 'decoded'), (self.zeromq_pub_msg_sink_0, 'in'))
+        self.msg_connect((self.adsb_demod_0, 'demodulated'), (self.adsb_decoder_0, 'demodulated'))
+        self.connect((self.adsb_demod_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.adsb_demod_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.adsb_framer_1, 0), (self.adsb_demod_0, 0))
+        self.connect((self.analog_const_source_x_0, 0), (self.qtgui_time_sink_x_0, 1))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.analog_noise_source_x_1, 0), (self.blocks_throttle_1, 0))
+        self.connect((self.analog_noise_source_x_1, 0), (self.blocks_selector_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.zeromq_pub_sink_0, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.zeromq_pub_sink_1, 0))
         self.connect((self.blocks_add_xx_1, 0), (self.zeromq_pub_sink_2, 0))
+        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.adsb_framer_1, 0))
         self.connect((self.blocks_delay_0, 0), (self.blocks_add_xx_1, 0))
+        self.connect((self.blocks_selector_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
+        self.connect((self.blocks_selector_0, 2), (self.blocks_throttle_1, 0))
+        self.connect((self.blocks_selector_0, 1), (self.rational_resampler_xxx_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.blocks_throttle_0, 0), (self.blocks_add_xx_1, 1))
         self.connect((self.blocks_throttle_1, 0), (self.blocks_add_xx_0, 0))
         self.connect((self.blocks_throttle_1, 0), (self.blocks_delay_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.analog_wfm_rcv_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "app")
@@ -126,12 +224,15 @@ class app(gr.top_block, Qt.QWidget):
     def set_threshold(self, threshold):
         self.threshold = threshold
         Qt.QMetaObject.invokeMethod(self._threshold_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.threshold)))
+        self.adsb_framer_1.set_threshold(self.threshold)
+        self.analog_const_source_x_0.set_offset(self.threshold)
 
     def get_select_index(self):
         return self.select_index
 
     def set_select_index(self, select_index):
         self.select_index = select_index
+        self.blocks_selector_0.set_output_index(self.select_index)
 
     def get_gain(self):
         return self.gain
@@ -144,6 +245,7 @@ class app(gr.top_block, Qt.QWidget):
 
     def set_fs(self, fs):
         self.fs = fs
+        self.qtgui_time_sink_x_0.set_samp_rate(int(self.fs))
 
     def get_fc(self):
         return self.fc
